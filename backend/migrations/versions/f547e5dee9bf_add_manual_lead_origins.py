7b724d5f34d4a19d6584a97090a8f32e477738d1
Otavio <otavio.grotto@gmail.com>
2026-05-09 14:05:37 -0300

    fix(migrations+repository): correct down_revision chain and remove deleted_at ref
    - f547e5dee9bf: fix down_revision from non-existent 'e7f8a9b0c1d2' to '313501e5eb7a'
    
    - lead_repository.py: remove LeadModel.deleted_at reference (model uses is_archived only)

--- backend/migrations/versions/f547e5dee9bf_add_manual_lead_origins.py
+++ backend/migrations/versions/f547e5dee9bf_add_manual_lead_origins.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'f547e5dee9bf'
-down_revision: Union[str, None] = 'e7f8a9b0c1d2'
+down_revision: Union[str, None] = '313501e5eb7a'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
